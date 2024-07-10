4af377f12709ef3118db46a1be59d067dc4534d3
Aaron Cuadrado <163054420+AaronCuadrado@users.noreply.github.com>
2024-07-10 09:09:15 +0000

    finalizado

--- src/app.js
+++ src/app.js
@@ -1,11 +1,19 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+// constantes con las opciones
+const pronombre = ["mi", "tu", "su"];
+const adjetivo = ["triste", "frustrante", "ridiculo"];
+const sustantivo = ["vida", "suerte", "problema"];
+const extensiones = [".com", ".es", ".net"];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+
+// forin para generar TODOS los dominios
+for (let i = 0; i < pronombre.length; i++) {
+  for (let j = 0; j < adjetivo.length; j++) {
+    for (let k = 0; k < sustantivo.length; k++) {
+      for (let l = 0; l < extensiones.length; l++) {
+        const dominio = `${pronombre[i]}${adjetivo[j]}${sustantivo[k]}${extensiones[l]}`;
+        console.log(dominio);
+      }
+    }
+  }
+}
